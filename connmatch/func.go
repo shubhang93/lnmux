b9ceaaa06bd0da473e1c439c9c11d2b050a86f18
Shubhang Balkundi <shubhang.sabbath@gmail.com>
2024-04-12 16:00:47 +0530

    adds docs

--- connmatch/func.go
+++ connmatch/func.go
@@ -2,4 +2,5 @@ package connmatch
 
 import lnmuxio "github.com/shubhang93/lnmux/io"
 
-type Func func(connPkr lnmuxio.ConnPeeker) (bool, error)
+// Func is the function signatures for connection matchers
+type Func func(connPkr lnmuxio.ConnPeeker) (matched bool, matchErr error)
